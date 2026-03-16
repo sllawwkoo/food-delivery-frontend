e97593d71856cc289cd1ee2149bb1e0eb1a3a199
Viacheslav Pyvovarov <68162839+sllawwkoo@users.noreply.github.com>
2026-03-16 23:45:35 +0200

    UPDATE: App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
 import { Suspense } from "react";
-import { useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/app/router";
 import { Loader } from "@/shared/ui/Loader";
 import { AppInit } from "./init/AppInit";
-import { selectAuthLoading } from "@/features/auth";
 
 function App() {
-  const loading = useSelector(selectAuthLoading);
 
   return (
     <>
